56cd64e5288a2a0b5ce9ded27a6d961919313ab8
Andi Prapugicu <prapugicu.andi02@yahoo.com>
2025-10-22 21:02:32 +0300

    settings mods

--- backend/settings.py
+++ backend/settings.py
@@ -134,7 +134,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    "https://finance-ledger-rho.vercel.app",
+]
 
 # REST Framework configuration
 REST_FRAMEWORK = {
